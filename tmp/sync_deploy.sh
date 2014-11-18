73b56a35bcc7ce4090a37f03e3c9db19feedbd27
Andrei Dragomir <adragomi@adobe.com>
2014-11-18 15:06:06 -0700

    Added script to help copy files

--- tmp/sync_deploy.sh
+++ tmp/sync_deploy.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 host=$1
-rsync -ravz -e 'ssh -p 2222' --exclude '.git' ~/work/s/deploy/puppet root@${host}:~/saasbase-deployment/
-rsync -ravz -e 'ssh -p 2222' --exclude '.git' ~/work/trommel/clusters root@${host}:~/clusters/
+
+for host in $*; do
+  rsync -ravz --exclude '.git' ~/work/s/deploy/puppet root@${host}:~/saasbase-deployment/
+  rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/clusters
+
+  for module in puppet-docker puppet-mesos puppet-etcd puppet-marathon; do 
+    rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/puppet/
+  done
+done
