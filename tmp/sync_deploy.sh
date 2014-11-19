8492bfda8a21a9ab038ee2d1c4652118368f415a
Andrei Dragomir <adragomi@adobe.com>
2014-11-19 13:12:32 -0700

    Moved to gnu parallel

--- tmp/sync_deploy.sh
+++ tmp/sync_deploy.sh
@@ -6,11 +6,11 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+export DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-host=$1
-
-for host in $*; do
+copy_stuff() {
+  local host=$1
+  echo "Copying to $host"
   rsync -ravz --exclude '.git' ~/work/s/deploy/puppet root@${host}:~/saasbase-deployment/
   rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/metalcell/clusters
   #rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/clusters
@@ -19,4 +19,8 @@ for host in $*; do
     rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/metalcell/puppet/
     #rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/puppet/
   done
-done
+}
+
+export -f copy_stuff
+
+parallel --env DIR copy_stuff ::: $*
