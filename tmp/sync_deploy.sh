d724d5f95732e60611e19af5a9329068a9cce2ad
Andrei Dragomir <adragomi@adobe.com>
2014-11-19 13:44:35 -0700

    Fixed hardcoded paths

--- tmp/sync_deploy.sh
+++ tmp/sync_deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export SAASBASE_DEPLOYMENT_HOME=${SAASBASE_DEPLOYMENT_HOME:-~/work/s/deploy/puppet}
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -11,16 +13,14 @@ export DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 copy_stuff() {
   local host=$1
   echo "Copying to $host"
-  rsync -ravz --exclude '.git' ~/work/s/deploy/puppet root@${host}:~/saasbase-deployment/
+  rsync -ravz --exclude '.git' $SAASBASE_DEPLOYMENT_HOME root@${host}:~/saasbase-deployment/
   rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/metalcell/clusters
-  #rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/clusters
 
   for module in puppet-docker puppet-mesos puppet-etcd puppet-marathon; do 
     rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/metalcell/puppet/
-    #rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/puppet/
   done
 }
 
 export -f copy_stuff
 
-parallel --env DIR copy_stuff ::: $*
+parallel --env DIR --env SAASBASE_DEPLOYMENT_HOME copy_stuff ::: $*
