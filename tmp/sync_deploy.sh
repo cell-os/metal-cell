776800338457fb8e7e6a8e5f9000e120cbeec5e5
Andrei Dragomir <adragomi@adobe.com>
2014-11-18 21:37:14 -0700

    Fixed location for the sync_deploy script

--- tmp/sync_deploy.sh
+++ tmp/sync_deploy.sh
@@ -12,9 +12,11 @@ host=$1
 
 for host in $*; do
   rsync -ravz --exclude '.git' ~/work/s/deploy/puppet root@${host}:~/saasbase-deployment/
-  rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/clusters
+  rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/metalcell/clusters
+  #rsync -ravz --exclude '.git' $DIR/../../clusters/* root@${host}:~/clusters
 
   for module in puppet-docker puppet-mesos puppet-etcd puppet-marathon; do 
-    rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/puppet/
+    rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/metalcell/puppet/
+    #rsync -ravz --exclude '.git' $DIR/../../$module root@${host}:~/puppet/
   done
 done
