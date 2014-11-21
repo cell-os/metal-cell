8c83c1387a589da3e69bd06bc6a28e4d87fba264
Adrian Muraru <amuraru@adobe.com>
2014-11-20 20:12:11 -0700

    Added Bosun collector

--- monitoring/bosun/push2opentsdb.sh
+++ monitoring/bosun/push2opentsdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true ; do 
-    echo "put my.metric `date +%s` $(($RANDOM % 255))  host=somehost"  | nc scvm1278.dev.ut1.omniture.com 31666
+    echo "put my.metric `date +%s` $(($RANDOM % 255))  host=somehost"  | nc scvm1251.dev.ut1.omniture.com 31666
     echo "pushed"
     sleep 1;
 done
