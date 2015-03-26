3009f8b29a37f92ab1048c6a882429e26c3ab05e
Cosmin Lehene <clehene@gmail.com>
2015-03-26 16:19:43 -0700

    [INCOMPLETE] - bash-lb refactoring

--- discovery/bash-lb.sh
+++ discovery/bash-lb.sh
@@ -3,19 +3,19 @@
 app=$1
 port=$2
 
-export ETCDCTL_PEERS=http://scvm1231.dev.ut1.omniture.com:4001,http://scvm1231.dev.ut1.omniture.com:4001,http://scvm1235.dev.ut1.omniture.com:4001
 
+export ETCDCTL_PEERS=http://scvm1231.dev.ut1.omniture.com:4001,http://scvm1231.dev.ut1.omniture.com:4001,http://scvm1235.dev.ut1.omniture.com:4001
+ETCDCTL=./etcd-v0.5.0-alpha.4-linux-amd64/etcdctl 
 port=$(curl http://scvm1231.dev.ut1.omniture.com:8080/v2/tasks 2>/dev/null | jq --raw-output ".tasks[] | select(.appId == \"/$app\") | .ports[0] " | head -n 1 | tr -d ' ')
 
 while true
 do
 
-counter=1
-./etcd-v0.5.0-alpha.4-linux-amd64/etcdctl --peers "http://scvm1231.dev.ut1.omniture.com:4001,http://scvm1232.dev.ut1.omniture.com:4001,http://scvm1235.dev.ut1.omniture.com:4001" ls \
+$ETCDCTL ls \
     /skydns/local/cell/$app > removal
 
 while read line; do
-  entry=$(./etcd-v0.5.0-alpha.4-linux-amd64/etcdctl get $line)
+  entry=$($ETCDCTL get $line)
   echo $entry| cut -d "\"" -f 4,7 | sed "s/\"//;s/}//;s/\ //" | xargs curl -s -I --connect-timeout 2 >/dev/null 2>/dev/null
   if [ $? -ne 0 ]; then
     echo "Remvoing $line -> $entry from DNS"
