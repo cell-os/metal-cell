388af4ab97e9a89384615670f5d8f1d47b1468bc
Dragos Dascalita Haut <ddascal@adobe.com>
2016-01-08 11:27:52 -0800

    [CELL-186] - DNS - A Record Alias support
    Add support in the API for a payload that includes the hosted zone ID of the ELB:
    ```
    {"cell_name":"my-cell",  "values":["my-ELB-domain-name"], "hostedZoneId":"ELB-domain-name-zone-id"}
    ```
    
    For backwards compatibility the `values` field is still used, but when a `hostedZoneId` is provided the function assumes that we want to create an Alias A Record using `values[0]` and `hostedZoneId`

--- discovery/lambda/register-gateway-set.js
+++ discovery/lambda/register-gateway-set.js
@@ -6,25 +6,66 @@ var GW_PREFIX = '*.gw.';
 
 console.log('metal-cell dns');
 
+function getParamsForARecord(values, dnsName) {
+    var params = {
+        ChangeBatch: {
+          Changes: [{
+              Action: 'UPSERT',
+              ResourceRecordSet: {
+                Name: dnsName,
+                Type: 'A',
+                TTL: 10,
+                ResourceRecords: values.map( function(ip) {
+                    return { Value: ip };
+                })
+              }
+            }]
+        },
+        HostedZoneId: ZONE_ID 
+  };
+  return params;
+}
+
+function getParamsForAliasRecord(aliasDnsName, aliasHostedZoneId, dnsName) {
+    var params = {
+        ChangeBatch: {
+          Changes: [{
+              Action: 'UPSERT',
+              ResourceRecordSet: {
+                Name: dnsName,
+                Type: 'A',
+                AliasTarget: {
+                    DNSName: aliasDnsName,
+                    HostedZoneId: aliasHostedZoneId,
+                    EvaluateTargetHealth: true
+                }
+              }
+            }]
+        },
+        HostedZoneId: ZONE_ID 
+  };
+  return params;      
+}
+
 exports.handler = function(event, context) {
   
   var dnsName =  GW_PREFIX + event.cell_name + DOMAIN;
-  var params = {
-    ChangeBatch: {
-      Changes: [{
-          Action: 'UPSERT',
-          ResourceRecordSet: {
-            Name: dnsName,
-            Type: 'A',
-            TTL: 10,
-            ResourceRecords: event.values.map( function(ip) {
-                return { Value: ip };
-            })
-          }
-        }]
-    },
-    HostedZoneId: ZONE_ID 
-  };
+  var params = {};
+  
+  // {"cell_name":"my-cell",  "values":["my-ELB-domain-name"], "hostedZoneId":"ELB-domain-name-zone-id"}
+  if ( typeof(event.hostedZoneId) !== "undefined" && event.hostedZoneId !== null ) {
+      if ( typeof(event.values) === "undefined" || event.values === null ) {
+          return context.fail("'values' field is missing. ");
+      }
+      if ( Object.prototype.toString.call(event.values) !== "[object Array]" || event.values.length !== 1 ) {
+          return context.fail("'values' field expects a one element array for now. ");
+      }
+      params = getParamsForAliasRecord( event.values[0], event.hostedZoneId, dnsName );
+  } else {
+      params = getParamsForARecord(event.values, dnsName );    
+  }
+  
+  console.log("Calling R53 with params:", JSON.stringify(params, null, 2));
 
   route53.changeResourceRecordSets(params, function(err, data) {
     if (err) {
